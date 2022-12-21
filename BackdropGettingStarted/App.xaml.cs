03a26d6b3211a9cfd50b0ef7c3d43199a2c47045
BalachandarSelvam <balachandar.selvam@syncfusion.com>
2022-12-21 18:19:36 +0530

    Initial commit

---
+++ BackdropGettingStarted/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace BackdropGettingStarted;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new NavigationPage(new BackdropSamplePage());
+	}
+}
